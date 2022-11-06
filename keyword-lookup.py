9c01c13c672d245d94bdc768eb190a656cf7cf2f
Rukosa <98932585+Rukosa@users.noreply.github.com>
2022-11-06 14:30:01 -0800

    Updated formatting and planning

--- keyword-lookup.py
+++ keyword-lookup.py
@@ -1,46 +1,42 @@
-import nltk #Speech tagging. https://www.geeksforgeeks.org/part-speech-tagging-stop-words-using-nltk-python/
+import nltk  # Speech tagging. https://www.geeksforgeeks.org/part-speech-tagging-stop-words-using-nltk-python/
 nltk.download('averaged_perceptron_tagger')
+
+# Weight questions with noun keywords... Ex: "Where" should be weighted to all "Where" questions..
+# This should reduce lookup time as we won't need to go over every question.
+# How should we weight? Could weight in CSV file manually or do an initial sort here on bootup of the bot..
 
 class KeywordLookup:
     def __init__(self, keywords):
         self.keywords = keywords
         self.tag = ""
-        self.wordlist = [] #Our txt holding words from questions
-        self.questionList = [] #Every CSV question
-
+        self.wordlist = []  # Our txt holding words from questions
+        self.questionList = []  # Every CSV question
         self.mostMatched = []
 
     def lookup(self, keyword):
-        #lookup word with nltk and tag doing this to track keywords..
+        # lookup word with nltk and tag doing this to track keywords..
         if keyword not in self.wordlist:
             text = keyword
             word = nltk.pos_tag()
             val = word[0][1]
-            #add to file with word and tag
+            # add to file with word and tag
 
     def matching(self):
-        #Find if in our keywords document.. Make a txt with all nouns and verbs implemented in CSV
+        # Find if in our keywords document.. Make a txt with all nouns and verbs implemented in CSV
         for question in self.questionList:
             for word in self.keywords.split():
                 if word in self.wordlist:
                     self.match(self, question)
         # Tells us which question input matches most
         matchedQuestion = self.mostMatched.index(max(self.mostMatched))
-        print(self.questionList[matchedQuestion]) #The matched question
-        #print(answer-to-question)
-
-
-
+        print(self.questionList[matchedQuestion])  # The matched question
+        # print(answer-to-question)
 
     def match(self, question):
-        #Get question from CSV and check which ones match the most
+        # Get question from CSV and check which ones match the most
         splitText = self.keywords.split()
         matches = 0
         for word in splitText:
             if word in question:
                 matches = matches + 1
         self.mostMatched.append(matches)
-
-
-
-
